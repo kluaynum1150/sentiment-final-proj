85469c9338c647efbbfc05bd830748d32892a900
kluaynum951 <kluaynum951@gmail.com>
2022-05-30 04:38:14 +0700

    deploy final

---
+++ dist/js/about-legacy.5564ebf0.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkfinal_proj"]=self["webpackChunkfinal_proj"]||[]).push([[443],{4613:function(e,n,t){t.r(n),t.d(n,{default:function(){return i}});var u=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"about"},[t("h1",[e._v("This is an about page")])])}],l=t(3736),r={},s=(0,l.Z)(r,u,a,!1,null,null,null),i=s.exports}}]);
+//# sourceMappingURL=about-legacy.5564ebf0.js.map
